d5b8e991ae4317ec470239d8d0bd2191bca9a06d
Chen Liang <code@chen.technology>
2015-03-13 16:51:37 -0700

    [Connection] added retry supports

--- test/test.connection.js
+++ test/test.connection.js
@@ -8,6 +8,9 @@
 /*!
  * Module dependencies.
  */
+var Promise = require('bluebird');
+var _ = require('lodash');
+var debug = require('debug')('broker:test:connection');
 var Connection = require('./../lib/connection.js');
 var Producer = require('./../lib/producer');
 var Consumer = require('./../lib/consumer');
@@ -85,6 +88,70 @@ describe('Connection', function () {
         .should.notify(done);
     });
   });
+  describe('#ensureConnection(options)', function () {
+    this.timeout(20 * 1000);
+    var originalEstablishConnection;
+    beforeEach(function () {
+      this.connection = new Connection(this.testOptions);
+      originalEstablishConnection = this.connection._establishConnection;
+      sinon.stub(this.connection, '_establishConnection');
+    });
+    afterEach(function () {
+      this.connection._establishConnection.restore();
+      this.connection.close();
+      this.connection = null;
+    });
+    describe('with `maxRetries`', function () {
+      beforeEach(function () {
+        this.connection.uri = 'amqp://localhost:5672';
+        this.connection._establishConnection
+          .returns(
+            originalEstablishConnection.bind(this.connection)()
+          );
+      });
+      it('will not call `_establishConnection` more than maxRetries', function (done) {
+        var self = this;
+        var maxRetries = 2;
+        this.connection.ensureConnection({
+            maxRetries: maxRetries
+          })
+          .should.be.rejectedWith('max retries 2 reached')
+          .then(function () {
+            self.connection._establishConnection.callCount.should.equal(maxRetries);
+          })
+          .should.notify(done);
+      });
+    });
+    describe('if established after retry three times', function () {
+      beforeEach(function () {
+        var self = this;
+        var originalUri = this.connection.uri;
+        this.connection.uri = 'amqp://localhost:5672';
+        this.connection._establishConnection
+          .returns(
+            originalEstablishConnection.bind(this.connection)()
+          );
+        this.connection._establishConnection
+          .onCall(2)
+          .returns((function () {
+            self.connection.uri = originalUri;
+            return originalEstablishConnection.bind(self.connection)();
+          }).bind(this.connection)());
+      });
+      it('will resolve(connection)', function (done) {
+        var self = this;
+        var maxRetries = 4;
+        this.connection.ensureConnection({
+            maxRetries: maxRetries
+          })
+          .should.be.fulfilled
+          .then(function () {
+            self.connection._establishConnection.callCount.should.equal(3);
+          })
+          .should.notify(done);
+      });
+    });
+  });
   describe('#connect()', function () {
     beforeEach(function () {
       this.connection = new Connection(this.testOptions);
@@ -104,6 +171,28 @@ describe('Connection', function () {
         })
         .should.notify(done);
     });
+    describe('when wrong uri', function () {
+      this.timeout(30 * 1000);
+      beforeEach(function () {
+        var wrongOptions = _.clone(this.testOptions);
+        wrongOptions.uri = 'amqp://localhost:5672';
+        wrongOptions.autoRetry = true;
+        wrongOptions.maxRetries = 4;
+        this.connection = new Connection(wrongOptions);
+        sinon.spy(this.connection, '_establishConnection');
+      });
+      afterEach(function () {
+        this.connection._establishConnection.restore();
+      });
+      it('auto retries and calls _establishConnection more than once', function (done) {
+        var self = this;
+        this.connection.connect()
+          .catch(function (err) {
+            self.connection._establishConnection.callCount.should.equal(4);
+          })
+          .should.notify(done);
+      });
+    });
   });
   describe('#close()', function () {
     beforeEach(function () {
@@ -111,7 +200,7 @@ describe('Connection', function () {
       return this.connection.connect();
     });
     afterEach(function () {
-      if (this.connection._connection) {
+      if (this.connection.isConnected()) {
         return this.connection._connection.close();
       }
     });
@@ -127,6 +216,14 @@ describe('Connection', function () {
         })
         .should.be.fulfilled
         .should.notify(done);
+    });
+    it('emits `close` event', function (done) {
+      var self = this;
+      expect(this.connection._connection).to.exist;
+      this.connection.on('close', function (event) {
+        done();
+      });
+      this.connection.close();
     });
   });
   describe('#channel()', function () {
@@ -160,33 +257,33 @@ describe('Connection', function () {
         return this.connection._connection.close();
       }
     });
-    it('resolve new channel and set to _defaultChannel', function(done) {
+    it('resolve new channel and set to _defaultChannel', function (done) {
       var self = this;
       expect(this.connection._defaultChannel).to.not.exist;
       this.connection.getDefaultChannel()
-      .should.be.fulfilled
-      .then(function(createdChannel) {
-        expect(self.connection._defaultChannel).to.exist;
-      })
-      .should.notify(done);
+        .should.be.fulfilled
+        .then(function (createdChannel) {
+          expect(self.connection._defaultChannel).to.exist;
+        })
+        .should.notify(done);
     });
   });
-  describe('#Producer(options)', function() {
-    before(function() {
+  describe('#Producer(options)', function () {
+    before(function () {
       this.connection = new Connection(this.testOptions);
     });
-    it('will return an instanceof Producer', function(done) {
+    it('will return an instanceof Producer', function (done) {
       var producer = this.connection.Producer();
       producer.should.be.an.instanceof(Producer);
       expect(producer.channel).to.exist;
       done();
     });
   });
-  describe('#Consumer(options)', function() {
-    before(function() {
+  describe('#Consumer(options)', function () {
+    before(function () {
       this.connection = new Connection(this.testOptions);
     });
-    it('will return an instanceof Consumer', function(done) {
+    it('will return an instanceof Consumer', function (done) {
       var consumer = this.connection.Consumer();
       consumer.should.be.an.instanceof(Consumer);
       expect(consumer.channel).to.exist;
